3890cde57129c056d6d499904c4804847e2adbeb
Alyssa Carter <trillionalyssa@gmail.com>
2014-03-20 23:58:23 +0000

    Initial commit

---
+++ xmonad.nix
@@ -0,0 +1,25 @@
+{config, pkgs, ...}:
+
+{
+  environment.systemPackages = with pkgs; [
+    wine
+    dmenu
+    scrot
+    haskellPackages.xmobar
+    haskellPackages.xmonad
+    haskellPackages.xmonadContrib
+    haskellPackages.xmonadExtras
+    trayer
+  ];
+
+  services.xserver = {
+    enable = true;
+    layout = "us";
+    windowManager.xmonad.enable = true;
+    windowManager.default = "xmonad";
+    desktopManager.xterm.enable = false;
+    desktopManager.default = "none";
+  };
+  
+  services.nixosManual.showManual = true;
+}
